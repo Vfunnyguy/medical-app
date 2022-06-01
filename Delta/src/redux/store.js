c9bd8a4c50493651e1b25e7875434e44c48859e2
Vfunnyguy <nghoangviet2000@hotmail.com>
2022-06-01 20:19:43 +0700

    rename

--- Delta/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore,applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import rootReducer from './reducer/index';
-import { composeWithDevTools } from "redux-devtools-extension";
-const store=configureStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(thunk))
-)
-export default store
