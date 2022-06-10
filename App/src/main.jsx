724f4df8f9a8d2964407ef571bb47d73e9655bfd
Vfunnyguy <nghoangviet2000@hotmail.com>
2022-06-11 00:21:02 +0700

    add chatbot folder

--- App/src/main.jsx
+++ App/src/main.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import { Provider } from 'react-redux'
-import './styles/index.scss'
-import './styles/custom-mui.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import './styles/index.scss';
+import './styles/custom-mui.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-    <App />
-{/* <Provider store={store}>
-</Provider> */}
-  
+    {/* <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}> */}
+        <App />
+      {/* </Provider>
+    </PersistGate> */}
   </React.StrictMode>
-)
+);
